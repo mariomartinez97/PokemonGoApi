a37899862f2e212bc3184d8a5e4ef1deb8696a1a
mariomartinez97 <m4mariomiguel@gmail.com>
2017-09-30 19:25:01 -0400

    reading of pokemons and evolutions working with basic UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,16 @@ import { PokemonEvolutions } from './pokemon-evolution';
 export class AppComponent {
   pokemon: Pokemon[] = [];
   pokemonTest: PokemonEvolutions;  
+  pokemons: PokemonEvolutions[] = [];
+
+
   isLoading: boolean = false;
   error: boolean = false;
   babyPokemons: string[] = [];
-  links : string[]=[];
+  links : string[] = [];
+  showP: boolean = true;
+  showB: boolean = false;
+  bag: PokemonEvolutions[] = [];
 
   constructor(
     private pokemonService: PokemonService,
@@ -33,70 +39,49 @@ export class AppComponent {
 
   loadMore() {
     console.log("is loading");
-    // this.isLoading = true;
-
-    // this.pokemonService.getPokemon(this.pokemon.length, 151)
-    //   .then(pokemon => {
-    //     pokemon = pokemon.map(p => {
-    //       return p;
-    //     });
-    //     this.pokemon = this.pokemon.concat(pokemon);
-    //     this.isLoading = false;
-    //     this.error = false;
-    //     console.log(this.pokemon);
-    //   })
-    //   .catch(() => {
-    //     this.error = true;
-    //     this.isLoading = false;
-    //     console.log("ERROR");
-    //   });      
-      
       this.test();
   }
 
-//1-78 llamadas
+  test(){
+    for (let j = 4; j < 8; j++){
+      this.pokemonEvolutionService.getPokemon(j)
+      .then(b => {
+        this.pokemonTest = b
+          this.pokemons.push(this.pokemonTest);      
+        // console.log(this.pokemonTest);
+        // console.log("Baby Polemons names");
+        // console.log(this.babyPokemons);
+        });   
+      }     
+    }
 
+    addToBag(pokemon: PokemonEvolutions){
+      if(!this.bag.some(y => y.species.name == pokemon.species.name)){
+        this.bag.push(pokemon);
+        console.log(pokemon.species.name + "has been added");
+        console.log(this.bag)
+        console.log(this.bag[1].evolves_to[0].species.name);                     //read firt evolution name
+        console.log(this.bag[1].evolves_to[0].evolution_details[0].min_level);   //read first evolution min level
+      }
+      else{
+         console.log ("This is on the bag");
+         }
 
-//   test(){
-//     var i = 1;   
-//     var base = 'https://pokeapi.co/api/v2/evolution-chain/';
-//     while(i < 3) { 
-//       var link = base + i 
-//       console.log(link);
-//       this.pokemonEvolutionService.getPokemon(link)
-//       .then(a => {
-//         this.pokemonTest = a
-//         this.babyPokemons.push(this.pokemonTest.species.name);
-//         console.log(this.pokemonTest);        
-//         i++;
-//       });        
-//     }  
-//   }
-// }
-test(){
-    var base = 'https://pokeapi.co/api/v2/evolution-chain/1/';
-    var base1 = 'https://pokeapi.co/api/v2/evolution-chain/2/';
-    var base2 = 'https://pokeapi.co/api/v2/evolution-chain/3/';
-    var base3 = 'https://pokeapi.co/api/v2/evolution-chain/4/';
-    var base4 = 'https://pokeapi.co/api/v2/evolution-chain/5/';
-    var base5 = 'https://pokeapi.co/api/v2/evolution-chain/6/';
+    }
   
-    this.links.push(base);    
-    this.links.push(base1);    
-    this.links.push(base2);    
-    this.links.push(base3);    
-    this.links.push(base4);    
-    this.links.push(base5);    
-
-  for (let j = 0; j < this.links.length; j++){
-    console.log(this.links[j]);
-    this.pokemonEvolutionService.getPokemon(this.links[j])
-    .then(b => {
-      this.pokemonTest = b
-        this.babyPokemons.push(this.pokemonTest.species.name);      
-      console.log(this.pokemonTest);
-      });   
-    }     
+  evolution(){
+    // if(this.showP == true){
+    //   this.showP = false;
+    //   this.showB = true;
+    // }
+    // if(this.showB == true){
+    //   this.showB = false;
+    //   this.showP = true;
+    // }
+    console.log(this.pokemons);    
   }
+
+
+
 }
 
